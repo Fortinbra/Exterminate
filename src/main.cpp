160085f20340f46a02b2aa7fba4af1fa1e21cdcd
Fortinbra <fortinbra@becauseimclever.com>
2025-08-04 21:44:29 -0500

    Refactor CMakeLists.txt to enable UART output and remove blink.pio references from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
 
 #include <btstack_run_loop.h>
 #include <pico/cyw43_arch.h>
@@ -8,25 +7,11 @@
 
 #include "sdkconfig.h"
 #include "exterminate_platform.h"
-#include "blink.pio.h"
 
 // Sanity check
 #ifndef CONFIG_BLUEPAD32_PLATFORM_CUSTOM
 #error "Pico W must use BLUEPAD32_PLATFORM_CUSTOM"
 #endif
-
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
-
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
-}
-
-
 
 int main()
 {
